b16515733bce0731e7408b65907ce75a1570887a
Gunther <gunt.valentine@gmail.com>
2022-03-06 17:57:27 -0500

    commit tree 12d4bfb125f17bf66e3909e058e98ac1640c7839

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+multiply;
 
-// Write your code here
+let random;
+random = Number.parseInt(100, Math.random())
+random;
+
+const num3 = 24;
+const num4 = 6;
+const mod = num3 / num4;
+mod;
+
+let max;
+max = Math.max(1, 2, 3, 4, 5, 20);
+max;
